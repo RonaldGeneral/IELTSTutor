a20974d21a90c23b37ef73076a23e4ffca96525f
RonaldGeneral <gogo23993@gmail.com>
2024-09-06 13:34:16 +0800

    set up file structure

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
-const app = express()
+const expressLayouts = require('express-ejs-layouts')
 
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.use(expressLayouts)
+app.set('layout', './layouts/layout')
 app.set('view engine', 'ejs')
+
 
 app.get("/", (req, res) => {
     console.log("here")
@@ -12,4 +20,4 @@ const userRouter = require('./routes/users')
 
 app.use("/users", userRouter)
 
-app.listen(3000)
+app.listen(port, () => console.info(`App listening on port ${port}`))
